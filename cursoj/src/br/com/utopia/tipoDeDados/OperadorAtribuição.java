66a0c7d9dbc16598d98c62de87c7092a189c2ba1
Kleber Lima <kleber-jobs@outlook.com>
2016-11-14 00:33:57 -0200

    Registry new class
    create a new class of operators

---
+++ cursoj/src/br/com/utopia/tipoDeDados/OperadorAtribuição.java
@@ -0,0 +1,28 @@
+package br.com.utopia.tipoDeDados;
+
+public class OperadorAtribuição {
+
+	public static void main(String[] args) {
+		int numero =0;
+		int outroNumero = 15;
+		
+		//numero = numero + outroNumero;
+		numero += outroNumero;
+		System.out.println(numero);
+		
+		//Não podemos fazer
+/*		double d = 10; 
+		float f = d;
+		long l = d;
+		int i = d;*/
+		
+		
+		//podemos fazer
+		/*int i = 20;
+		long l = i;
+		float f = i;
+		double d = i;*/
+
+	}
+
+}
